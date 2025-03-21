2dff2ba5887f6581ba1d5c57ba2dc687bd42450b
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-03-21 19:27:28 +0100

    Modif fichier RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,14 +18,17 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'label' => 'Accepter les conditions',
+                'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
+
             ->add('plainPassword', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
