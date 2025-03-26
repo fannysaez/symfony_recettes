a621d0601330b9e471c91aad71c497e0ddc9ac88
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-03-26 11:26:50 +0100

    création d'une commande de stats

---
+++ src/Command/StatsCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Entity\Pants;  // Utilise Pants pour les produits
+use App\Entity\Category;  // Utilise Category pour les catégories
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:stats', description: 'Affiche les statistiques du site.')]
+class StatsCommand extends Command
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Récupération des repositories
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $pantsRepository = $this->entityManager->getRepository(Pants::class);  // Utilise Pants pour les produits
+        $categoryRepository = $this->entityManager->getRepository(Category::class);  // Utilise Category pour les catégories
+
+        // Nombre d'utilisateurs
+        $userCount = $userRepository->count([]);
+        $output->writeln("<info>Nombre d'utilisateurs :</info> $userCount");
+
+        // Nombre de pantalons
+        $pantsCount = $pantsRepository->count([]);
+        $output->writeln("<info>Nombre de pantalons :</info> $pantsCount");
+
+        // Liste des catégories
+        $categories = $categoryRepository->findAll();
+        $output->writeln("<info>Catégories de produits :</info>");
+        foreach ($categories as $category) {
+            // Si tu utilises getLabel(), sinon remplace avec getName()
+            $output->writeln(" - " . $category->getLabel());  
+        }
+
+        // Prix moyen des pantalons
+        $avgPrice = $this->entityManager->createQuery('SELECT AVG(p.price) FROM App\\Entity\\Pants p')->getSingleScalarResult();
+        $output->writeln("<info>Prix moyen des pantalons :</info> " . number_format((float)$avgPrice, 2, ',', ' ') . " €");
+
+        return Command::SUCCESS;
+    }
+}
